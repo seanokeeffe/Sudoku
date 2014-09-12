316ef5c7957339ddba9ecb1007cd68a695e91efa
seanokeeffe <seanokeeffe28@gmail.com>
2014-09-12 11:21:34 -0700

    One button, that logs it’s tag

--- Sudoku/HKSOViewController.m
+++ Sudoku/HKSOViewController.m
@@ -18,12 +18,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect mainFrame = self.view.frame;
+    CGFloat x = CGRectGetWidth(mainFrame) * 0.5;
+    CGFloat y = CGRectGetHeight(mainFrame) * 0.5;
+    
+    CGRect buttonFrame = CGRectMake(x, y, 100, 100);
+    UIButton* button = [[UIButton alloc] initWithFrame:buttonFrame];
+    [button setTitle:@"Button" forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor blueColor];
+    button.tag = 1;
+    
+    [self.view addSubview:button];
+    
+    [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlStateHighlighted];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) buttonPressed:(id)sender
+{
+    NSLog(@"Button %d was pressed", ((UIButton*)sender).tag);
 }
 
 @end
